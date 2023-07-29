badd2147cf5e1e0c6675e86e6eede3a43c7f7254
Taeuk Gwak <gwak2837@kakao.com>
2023-07-29 22:23:09 +0900

    Fix: ai API error

--- src/routes/analysis/sql/getPromptFromLoCom.sql
+++ src/routes/analysis/sql/getPromptFromLoCom.sql
@@ -1,9 +1,10 @@
 /* @name getPromptFromLoCom */
-SELECT sfrnd_code AS who_code,
+SELECT election.district AS who_code,
   finance.basis_date AS when_,
   field_code,
   sector_code,
   commitment.title
 FROM commitment
+  JOIN election ON election.id = commitment.election_id
   JOIN finance ON finance.commitment_id = commitment.id
   AND commitment.id = $1;
